57ef9ebfcd700f7bbb631c98b76c14d803832af7
Eriyanuari <eriyanuari11@gmail.com>
2021-10-28 20:26:51 +0700

    menambahkan Contoh1.php,Contoh2.php,Contoh3.php,Matakuliah.php,Welcome.php,index.html

---
+++ Contoh3.php
@@ -0,0 +1,13 @@
+<?php 
+class Contoh3 extends CI_Controller 
+{ 
+    public function index() 
+    {
+        echo "<h1>Fashion</h1>";
+        echo "Fashion Man
+              Lucky Man
+              Xman
+              Atros
+              Qtyb";
+    }
+}
